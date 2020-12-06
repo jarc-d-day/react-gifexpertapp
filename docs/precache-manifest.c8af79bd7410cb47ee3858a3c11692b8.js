6f12d1daf90ce4be2fabb379b8f03af426cf2da4
Joel Ramos <jarccorporation@gmail.com>
2020-12-06 13:29:56 -0600

    docs agregado

---
+++ docs/precache-manifest.c8af79bd7410cb47ee3858a3c11692b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b0e15ddad30069088268dc475b1004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0490e0f6168ef73fc887",
+    "url": "/static/css/main.199b93f1.chunk.css"
+  },
+  {
+    "revision": "a13e31d6699d5138f112",
+    "url": "/static/js/2.bed60b36.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.bed60b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0490e0f6168ef73fc887",
+    "url": "/static/js/main.5a2dc2dc.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
